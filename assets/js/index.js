fc9b868000bde9ce26615edf50b28c9d3c7dd704
Noah Kovacs <n.kovacs1@icloud.com>
2015-04-30 23:30:04 -0400

    Latest work

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+/**
+ *   Author: Noah Kovacs
+ *   Date: 30 April 2015
+ */
+
+
+
+
+function stringReverse(text) {
+    var revString;
+
+    revString = toString(text.reverse);
+    console.log(revString);
+
+    return revString;
+}
